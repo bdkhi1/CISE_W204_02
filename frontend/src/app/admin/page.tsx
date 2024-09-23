dcf08cb049682dd7a2bd267babca5ee8088530a4
Daniil-AUT <ryj5244@autuni.ac.nz>
2024-09-23 16:38:39 +1200

    [frontend][admin] Create admin route
    Create the initial admin route where the content and logic for the administrator operations will be located.

---
+++ frontend/src/app/admin/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import React from 'react';
+import AdminDashBoard from "@/components/Admin/AdminView";
+
+export default function AdminPage() {
+  return (
+    <main>
+        <AdminDashBoard />
+    </main>
+  );
+}
---
+++ frontend/src/components/Admin/AdminView.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react';
+
+export default function AdminPage() {
+  return (
+    <main>
+      <h1>Administrator Dashboard</h1>
+    </main>
+  );
+}
