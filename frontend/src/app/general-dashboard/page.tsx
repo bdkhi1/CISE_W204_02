c85d9945a63cd36ca1cb0108a69830333b2950ba
Lucas Hashemi <trn4115@aut.ac.nz>
2024-09-26 08:51:32 +1200

    feat(general-dashboard): add PopulatedNavBar
    Added the PopulatedNavBar component to GeneralDashboard

--- frontend/src/app/general-dashboard/page.tsx
+++ frontend/src/app/general-dashboard/page.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+// import NavBar from "../../components/nav/NavBar";
+// import NavItem from "../../components/nav/NavItem";
+import PopulatedNavBar from "@/components/PopulatedNavBar"; // Ensure the path is correct
 
 const GeneralDashboard = () => {
   return (
-    <div>
-      <h1>General Dashboard</h1>
-      <p>This is the general dashboard page.</p>
-    </div>
+    <>
+      <PopulatedNavBar />
+
+      <div>
+        <h1>General Dashboard</h1>
+        <p>This is the general dashboard page.</p>
+      </div>
+    </>
   );
 };
 
