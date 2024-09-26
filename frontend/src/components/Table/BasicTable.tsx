63e81c4ce32a2aad7df227185fff0b282529523b
Daniil-AUT <ryj5244@autuni.ac.nz>
2024-09-26 15:01:47 +1200

    [table][article] Create sort table function and modify article dt

--- frontend/src/components/Table/BasicTable.tsx
+++ frontend/src/components/Table/BasicTable.tsx
@@ -1,71 +1,81 @@
 import React, { useMemo } from 'react';
-import { useTable } from 'react-table';
-import { COLUMNS } from './columns'; // Your columns setup
-import useFetchArticles from './fetchArticles'; // Import the fetch hook
-import styles from './table.module.scss';  // Import the SCSS file
+import { useTable, useSortBy, Column, HeaderGroup, Row, Cell } from 'react-table';
+import { COLUMNS } from './columns'; 
+import useFetchArticles from './fetchArticles'; 
+import styles from './table.module.scss';  
 
-export const BasicTable = () => {
-    // Fetch the articles using the custom hook
+// The data type for the article that is to be manipulated for the table.
+interface Article {
+  id: string;
+  title: string;
+  authors: string;
+  source: string;
+  pubyear: string;
+  doi: string;
+  claim: string;
+  evidence: string;
+}
+
+export const BasicTable: React.FC = () => {
     const articles = useFetchArticles();
     
-    const columns = useMemo(() => COLUMNS, [])
-    const data = useMemo(()=> articles, [])
+    const columns = useMemo<Column<Article>[]>(() => COLUMNS, [])
+    const data = useMemo<Article[]>(() => articles, [articles])
     
-    // Render the table 
-    const tableInstance = useTable( {
-        columns,
-        data
-    }   
+    const tableInstance = useTable<Article>(
+        {
+            columns,
+            data
+        },
+        useSortBy
     )
 
-    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = tableInstance
+    const { 
+        getTableProps, 
+        getTableBodyProps, 
+        headerGroups, 
+        rows, 
+        prepareRow 
+    } = tableInstance
 
     return (
         <div className={styles.tableContainer}>
             <table {...getTableProps()} className={styles.darkThemeTable}>
-                {/* Header for the table */}
                 <thead>
-                    {
-                    // For each header group extract the props
-                    headerGroups.map(headerGroup => (
-                        <tr {...headerGroup.getHeaderGroupProps}>
-                            {
-                                headerGroup.headers.map( column => (
-                                    <th {...column.getHeaderProps}>
-                                        {
-                                            column.render('Header')
-                                        }
-                                    </th>
-                                ))
-                            }
-                        
-                    </tr>
-                    ))
-                    }
-                    
+                    {/*Headers for the table*/}
+                    {headerGroups.map((headerGroup: HeaderGroup<Article>) => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map((column: HeaderGroup<Article>) => (
+                                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                                    {column.render('Header')}
+                                    <span>
+                                        {column.isSorted ? column.isSortedDesc ? ' 🔽' : ' 🔼' : ''}
+                                    </span>
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
                 </thead>
-                {/* Body of the table */}
+                {/*Contents of the header for the each of the columns*/}
                 <tbody {...getTableBodyProps()}>
-                    {
-                        rows.map(row => {
-                            prepareRow(row)
-                            return (
-                                <tr {...row.getRowProps()}>
-                                    {
-                                        row.cells.map(cell => {
-                                            return <td {...cell.getCellProps}>
-                                                {cell.render('Cell')}
-                                            </td>
-                                        })
-                                    }
-                                    
-                                </tr>
-                            )
-                        })
-                    }
-                    
+                    {rows.map((row: Row<Article>) => {
+                        prepareRow(row)
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map((cell: Cell<Article>) => {
+                                    return (
+                                        <td {...cell.getCellProps()}>
+                                            {cell.render('Cell')}
+                                        </td>
+                                    )
+                                })}
+                            </tr>
+                        )
+                    })}
                 </tbody>
             </table>
         </div>
     )
 }
+
+export default BasicTable;
