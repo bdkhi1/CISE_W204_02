e781b4ff771cd67c63dbdd2299f606fe93b0eed8
Gonj3 <jonathan.downes123@gmail.com>
2024-10-02 16:02:00 +1300

    [fix] remove unused imports

--- frontend/src/components/ModeratorDashboard.tsx
+++ frontend/src/components/ModeratorDashboard.tsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PopulatedNavBar from './PopulatedNavBar';
-import Link from 'next/link';
 
 function ModeratorDashboard(){
 
