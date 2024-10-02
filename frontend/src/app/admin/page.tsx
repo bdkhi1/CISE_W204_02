62d7a908bbde63c1d76ca270367002516a8d4fa5
Daniil-AUT <ryj5244@autuni.ac.nz>
2024-10-03 08:57:32 +1300

    [admin][dashboard] Set up components

---
+++ frontend/src/app/admin/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react';
+import BasicTable from '@/components/Table/BasicTable';
+
+
+export default function AdminPage() {
+  return (
+    <main>
+      <BasicTable />
+    </main>
+  );
+}
