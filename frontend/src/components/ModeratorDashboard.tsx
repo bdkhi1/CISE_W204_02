692cdbdbd17e0ee7e5f6333ee81066a17fdb6351
Gonj3 <jonathan.downes123@gmail.com>
2024-10-02 16:58:44 +1300

    [feat] add table component to moderator dashboard

--- frontend/src/components/ModeratorDashboard.tsx
+++ frontend/src/components/ModeratorDashboard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PopulatedNavBar from './PopulatedNavBar';
+import BasicTable from './Table/BasicTable';
 
 function ModeratorDashboard(){
 
@@ -8,6 +9,7 @@ function ModeratorDashboard(){
             <PopulatedNavBar />
             <div className='ModeratorDashboard'>
                 <h1>Moderator Dashboard</h1>
+                <BasicTable />
             </div>
         </>
     )
