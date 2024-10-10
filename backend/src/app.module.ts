d8ecb4873f4bc7e0d5befec29f6896d4ff4f2028
Daniil1984195 <ryj5244@autuni.ac.nz>
2024-10-10 13:01:39 +1300

    [backend[update] Add Admin module to main

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,13 +5,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ArticleModule } from './api/articles/article.module';
 import { ConfigModule } from '@nestjs/config';
 import { ModerationModule } from './api/articles/moderation/moderation.module';
+import { AdministrationModule } from './api/articles/administration/administration.module'; 
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DB_URI),
     ArticleModule,
-    ModerationModule
+    ModerationModule,
+    AdministrationModule 
   ],
   controllers: [AppController],
   providers: [AppService],
