9687917e3c3ee9c287cdd4d111b20697c55d168c
Daniil1984195 <ryj5244@autuni.ac.nz>
2024-10-16 11:42:45 +1300

    [table][analyst] Create analyst table

--- frontend/src/components/analyst/AnalystDashboard.tsx
+++ frontend/src/components/analyst/AnalystDashboard.tsx
@@ -2,11 +2,13 @@
 
 import React from "react";
 import styles from "../dashboard.module.scss";
-
+import AnalystTable from "./AnalystTable/AnalystTable"
 const AnalystDashboard: React.FC = () => {
   return (
     <div className={styles.pageBackground}>
       <div className={styles.dashboardContainer}>
+
+        <AnalystTable />
         <h1 className={styles.dashboardTitle}>Analyst Dashboard</h1>
         <p className={styles.dashboardDesc}>
           This is a description of the Analyst Dashboard.
